145bc53e088448f33ce874dc19ea7e5e56c9a3ce
Eric Müller <mueller@kip.uni-heidelberg.de>
2014-09-25 09:53:32 +0200

    redirect stdout/stderr to files

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
+from os import open, close, dup, O_WRONLY, O_CREAT
+
+oldstdout = dup(1)
+close(1)
+open("stdout", O_WRONLY|O_CREAT)
+
+oldstderr = dup(2)
+close(2)
+open("stderr", O_WRONLY|O_CREAT)
+
+
+
 import os
 import shutil
 import plot_spikes
 import time
+
+
+
 
 from pyNN.utility import get_script_args
 simulator_name = get_script_args(1)[0]
@@ -25,3 +40,11 @@ if not os.path.isfile(filename_spikes):
     print 'No spike file found :('
 
 plot_spikes.plot(filename_spikes, filename_result_plot)
+
+close(2)
+dup(oldstderr) # should dup to 2
+close(oldstderr) # get rid of left overs
+
+close(1)
+dup(oldstdout) # should dup to 1
+close(oldstdout) # get rid of left overs
